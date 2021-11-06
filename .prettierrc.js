1804e170564b74f253701897c4e72e70523e00ab
Marko Arsic <marsicdev@gmail.com>
2021-11-06 09:12:55 +0100

    Add prettier and editorconfig

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    htmlWhitespaceSensitivity: 'css',
+    insertPragma: false,
+    jsxBracketSameLine: true,
+    jsxSingleQuote: false,
+    printWidth: 100,
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none',
+    useTabs: false,
+    parser: 'typescript'
+}
